d08ed3bac9b353642504362a06774f831b0b5683
Fábio Canestraro <fabiocanestararo@hotmail.com.br>
2021-11-29 00:02:15 -0300

    select enfermeiro

--- FRONT/src/app/components/views/atendimento/cadastrar-atendimento/cadastrar-atendimento.component.ts
+++ FRONT/src/app/components/views/atendimento/cadastrar-atendimento/cadastrar-atendimento.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { Atendimento } from "src/app/models/atendimento";
 import { AtendimentoService } from "src/app/services/atendimento.service";
+import { Enfermeiro } from 'src/app/models/enfermeiro';
+import { EnfermeiroService } from 'src/app/services/enfermeiro.service';
 
 @Component({
     selector: "app-cadastrar-atendimento",
@@ -11,10 +13,15 @@ import { AtendimentoService } from "src/app/services/atendimento.service";
 export class CadastrarAtendimentoComponent implements OnInit {
     paciente!: string;
     enfermeiro!: string;
+    enfermeiros: Enfermeiro[] = [];
 
-    constructor(private router: Router, private service: AtendimentoService) {}
+    constructor(private router: Router, private service: AtendimentoService, private enfermeiroService: EnfermeiroService) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.enfermeiroService.list().subscribe((enfermeiros) => {
+          this.enfermeiros = enfermeiros;
+        }) 
+      }
 
     cadastrar(): void {
         let atendimento: Atendimento = {
